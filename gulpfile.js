c55f506a63f1de6073068443e61870b3b04e452c
Alex Khant <grashupfer99@gmail.com>
2018-07-11 22:54:45 +0900

    refactored

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 'use strict'
 
-const gulp = require("gulp");
-const concat = require("gulp-concat");
+// Require modules
+const gulp = require("gulp"); 
+const concat = require("gulp-concat");  
 const uglify = require("gulp-uglify");
 const rename = require("gulp-rename");
 const sass = require("gulp-sass");
@@ -11,77 +12,53 @@ const cleanCSS = require('gulp-clean-css');
 const image = require("gulp-image");
 const browserSync = require("browser-sync").create();
 
-// gulp scripts
-gulp.task('concatScripts', ()=> {
+// Concatenate js scripts and create map files
+gulp.task('scripts', () => {
     return gulp.src([
-        'js/circle/autogrow.js',
-        'js/circle/circle.js',
+        'node_modules/jquery/dist/jquery.min.js',
+        'js/*.js', 
+        'js/**/*.js'
     ])
         .pipe(maps.init())
-        .pipe(concat('global.js'))
-        .pipe(maps.write('./'))
-        .pipe(gulp.dest('js'))
-});
-
-// all.min.js
-gulp.task('scripts', ['concatScripts'], ()=> {
-    return gulp.src('js/all.js')
+        .pipe(concat('all.min.js'))
         .pipe(uglify())
-        .pipe(rename('all.min.js'))
+        .pipe(maps.write('./'))
         .pipe(gulp.dest('dist/scripts'))
 });
 
+
 // css concat
-gulp.task('compileSass', ()=>{
+gulp.task('styles', () => {
     return gulp.src('sass/global.scss')
         .pipe(maps.init())
+        .pipe(rename('all.min.css'))
         .pipe(sass())
+        .pipe(cleanCSS())
         .pipe(maps.write('./'))
-        .pipe(gulp.dest('css'))
+        .pipe(gulp.dest('dist/styles'))
         .pipe(browserSync.stream())
 });
 
-gulp.task('styles', ['compileSass'], ()=>{
-    return gulp.src('css/global.css')
-        .pipe(cleanCSS())
-        .pipe(rename('all.min.css'))
-        .pipe(gulp.dest('dist/styles'))
-});
-
-// gulp watch sass
-gulp.task('watchSass', ()=>{
-    
-    browserSync.init({
-        server: './'
-    })
-
-    gulp.watch( ([
-        'sass/**/*.scss',
-        'sass/circle/**/*.sass'
-    ]), ['compileSass']);
+// Set browserSync server, watch and serve sass files
+gulp.task('serve', () => {
+    browserSync.init({server: 'dist'});
+    gulp.watch( ( 'sass/*.scss'), ['styles']);
 })
 
-// img optimization
-gulp.task('images', ()=> {
+// Image optimization
+gulp.task('images', () => {
     return gulp.src('images/*')
         .pipe(image())
         .pipe(gulp.dest('dist/content'))
-
 })
 
-
-
-// clean
-gulp.task('clean', ()=>{
+// Removal of designated files and directories
+gulp.task('clean', () => {
     del(['dist', 'js/all*.js*', 'css/global.css*']);
 });
 
-// build
-gulp.task('build', [
-    'scripts',
-    'styles',
-    'images'
-], ()=>{
+// Build the static website  
+gulp.task('build', ['scripts', 'styles', 'images'], () => {
     return gulp.src([
         'icons/*',
         'index.html'
@@ -89,10 +66,7 @@ gulp.task('build', [
         .pipe(gulp.dest('dist'))
 });
 
-//
-
-gulp.task('serve', ['watchSass']);
-
-gulp.task('default', ['clean', 'serve'], ()=>{
-    gulp.start('build');
+// Default task
+gulp.task('default', ['clean', 'build'], ()=>{
+    gulp.start('serve');
 })
